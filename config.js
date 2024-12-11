4e455e7a285ab3145cfa8c363a69c4985240ba37
Mohammad RANA <ruhulaminrana98@gmail.com>
2024-12-11 18:30:15 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   ownerUsernames: ["MOHAMMADRANA"],//Your Username
   ownerNumber: "+8801752865115",//Your Number
-  admin: [""], // your id
+  admin: ["5847031273"], // your id
   prefix: "/", // dont change prefix 
   telegramBotToken: "8162681362:AAFYKP2skeSS3KN1tzz_LcJD_i6MFYtNa6c",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
