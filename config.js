d7a7b1b2025d9781773089e7ca0bf0d3a6ebd73c
Mohammad RANA <ruhulaminrana98@gmail.com>
2024-12-11 18:22:26 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
-  ownerNumber: "+8801615298449",//Your Number
+  ownerUsernames: ["MOHAMMADRANA"],//Your Username
+  ownerNumber: "+8801752865115",//Your Number
   admin: ["5920596290"], // your id
   prefix: "/", // dont change prefix 
   telegramBotToken: "Your_Bot_Token",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "https://t.me/ar_rana07",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
