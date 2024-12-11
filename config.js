83132ce3985c3e3a023e9c67c9b963b1c0a2c8e5
Mohammad RANA <ruhulaminrana98@gmail.com>
2024-12-11 18:29:18 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   ownerUsernames: ["MOHAMMADRANA"],//Your Username
   ownerNumber: "+8801752865115",//Your Number
-  admin: ["5920596290"], // your id
+  admin: [""], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "Your_Bot_Token",//Your Bot Tokens
+  telegramBotToken: "8162681362:AAFYKP2skeSS3KN1tzz_LcJD_i6MFYtNa6c",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
